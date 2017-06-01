9bccd4324e61093cbd0f03ac2646c9e7602825b5
shanshanli <shaniavina@gmail.com>
2017-06-01 16:10:15 -0400

    Create *sum_list.py

---
+++ *sum_list.py
@@ -0,0 +1,73 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+        dummy = ListNode('-inf')
+        cur, carry = dummy, 0
+        
+        while l1 or l2:
+            val = carry
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            val, carry = val % 10, val / 10
+            cur.next = ListNode(val)
+            cur = cur.next
+            
+        if carry == 1:
+            cur.next = ListNode(1)
+            
+        return dummy.next
+        
+  @@@@follow up
+  # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        stk1, stk2 = [], []
+        while l1:
+            stk1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            stk2.append(l2.val)
+            l2 = l2.next
+        
+        sum = 0
+        pre, head = None, None
+        while stk1 or stk2:
+            sum /= 10
+            if stk1:
+                sum += stk1.pop()
+            if stk2:
+                sum += stk2.pop()
+                
+            head = ListNode(sum % 10)
+            head.next = pre
+            pre = head
+        if sum >= 10:
+            head = ListNode(sum / 10)
+            head.next = pre
+        return head
+            
